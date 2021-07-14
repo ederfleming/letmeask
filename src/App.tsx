2df4b2639e673b5778aa32aada9f0ff6b33047ba
Eder R. Fleming <ederfleming@gmail.com>
2021-07-14 14:28:43 -0300

    Clean folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import Main from 'components/Main';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/'>
-          <Main />
+        <Route exact path="/">
+          {/*   <Main /> */}
         </Route>
-        <Route path='/about'>
-          <Main description='This is the About Page' />
+        <Route path="/about">
+          {/* <Main description="This is the About Page" /> */}
         </Route>
       </Switch>
     </Router>
